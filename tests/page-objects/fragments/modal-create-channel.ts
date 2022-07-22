c1e0c5bcc7de55544c14b565d88c59953c8c3ae7
souzaramon <ramon.souza@objective.com.br>
2022-07-22 07:51:07 -0300

    feat: create locators

--- tests/page-objects/fragments/modal-create-channel.ts
+++ tests/page-objects/fragments/modal-create-channel.ts
@@ -7,23 +7,23 @@ export class ModalCreateChannel {
     this.page = page;
   }
 
+  // CSS Selector
+  // https://www.w3schools.com/cssref/css_selectors.asp
   get inputName() {
-    return this.page.locator('');
+		return this.page.locator('#modal-root [placeholder="Channel Name"]');
   }
 
-  get inputTopic() {
-    return this.page.locator('');
-  }
-
+  // XPath Selector
+  // https://www.w3schools.com/xml/xpath_syntax.asp
   get checkboxPrivate() {
-    return this.page.locator('');
+    return this.page.locator(
+			'//*[@id="modal-root"]//*[contains(@class, "rcx-field") and contains(text(), "Private")]/../following-sibling::label/i',
+		);
   }
 
-  get buttonCancel() {
-    return this.page.locator('');
-  }
-  
+  // CSS Selector + Playwright Text Selector
+  // https://playwright.dev/docs/selectors#text-selector 
   get buttonCreate() {
-    return this.page.locator('');
+    return this.page.locator('#modal-root button:has-text("Create")');
   }
 }
